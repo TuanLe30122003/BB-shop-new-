ea1971d968ca5d63b06dc5b15428e3dfa36ad6eb
TuanLe30122003 <91524625+TuanLe30122003@users.noreply.github.com>
2022-09-20 13:26:34 +0700

    20/9/22 Third commit (1st on this repo because of errors in some works)

--- main.js
+++ main.js
@@ -7,15 +7,40 @@ const humanBtn = document.querySelector('#human');
 const stageBtn = document.querySelectorAll('.stage');
 
 const homeStage = document.querySelector('#home-stage');
+const preOrderStage = document.querySelector('#preOrder-stage');
+const aboutStage = document.querySelector('#about-stage');
 
-// for(var i = 0; i < stageBtn.length; i++) {
-//     stageBtn[i].onclick = function(e) {
 
-//     }
-// }
+// click on button to open the stage ********************
 
 homeBtn.onclick = function(e) {
-    homeStage.classList.toggle('appear');
-} 
+    for(var i = 0; i < stageBtn.length; i++) {
+        stageBtn[i].style.display = "none";
+    }
 
-homeStage.classList.toggle('appear');
+    homeStage.style.display = 'block';
+}
+
+preOrderBtn.onclick = function(e) {
+    for(var i = 0; i < stageBtn.length; i++) {
+        stageBtn[i].style.display = "none";
+    }
+
+    preOrderStage.style.display = 'block';
+}
+
+aboutBtn.onclick = function(e) {
+    for(var i = 0; i < stageBtn.length; i++) {
+        stageBtn[i].style.display = "none";
+    }
+
+    aboutStage.style.display = 'block';
+}
+// 
+
+homeBtn.click();
+
+const input = document.querySelectorAll('.form_content--unit input');
+const label = document.querySelectorAll('.form_content--unit label');
+
+console.log(input, label);
